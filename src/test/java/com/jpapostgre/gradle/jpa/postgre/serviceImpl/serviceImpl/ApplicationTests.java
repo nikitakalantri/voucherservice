d80779ecc53d7922866362af27f7c3399844212a
Rupzzz <62932574+Rupzzz@users.noreply.github.com>
2020-04-15 16:15:38 +0530

    Add files via upload

---
+++ src/test/java/com/jpapostgre/gradle/jpa/postgre/serviceImpl/serviceImpl/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpapostgre.gradle.jpa.postgre.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
